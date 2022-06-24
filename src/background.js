d8c405e8f1202cd142da83377c58a6d6cfc92181
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-06-24 14:10:13 +0800

    added event listener for tab change
    still buggy

--- src/background.js
+++ src/background.js
@@ -3,12 +3,28 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
   chrome.tabs.get(activeInfo.tabId, (tab) => {
     if (tab.url) {
       chrome.action.setPopup({
+        tabId: tab.id,
         popup: "src/popup.html",
       });
     } else {
       chrome.action.setPopup({
+        tabId: tab.id,
         popup: "src/popup-disabled.html",
       });
     }
   });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url) {
+    chrome.action.setPopup({
+      tabId: tabId,
+      popup: "src/popup.html",
+    });
+  } else {
+    chrome.action.setPopup({
+      tabId: tabId,
+      popup: "src/popup-disabled.html",
+    });
+  }
 });
