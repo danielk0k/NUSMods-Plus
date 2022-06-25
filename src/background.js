c43a29fa5cb492274c786de5e7eac127195ea71f
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-06-25 09:52:32 +0800

    fix bug when enabling or disabling popup
    added regex for the url

--- src/background.js
+++ src/background.js
@@ -1,22 +1,6 @@
 // Listen to URL changes on the active tab
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  chrome.tabs.get(activeInfo.tabId, (tab) => {
-    if (tab.url) {
-      chrome.action.setPopup({
-        tabId: tab.id,
-        popup: "src/popup.html",
-      });
-    } else {
-      chrome.action.setPopup({
-        tabId: tab.id,
-        popup: "src/popup-disabled.html",
-      });
-    }
-  });
-});
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (tab.url) {
+  if (isURLNUSMods(changeInfo.url || tab.url)) {
     chrome.action.setPopup({
       tabId: tabId,
       popup: "src/popup.html",
@@ -28,3 +12,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     });
   }
 });
+
+function isURLNUSMods(url) {
+  const regex = new RegExp(String.raw`(https\:\/\/nusmods\.com.)`);
+  return regex.test(url);
+}
